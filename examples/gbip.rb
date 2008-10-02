e7a86aa559cdc4c05f236a536e844932560b783e
James Healy <jimmy@deefa.com>
2008-10-02 21:28:45 +1000

    update example

--- examples/gbip.rb
+++ examples/gbip.rb
@@ -3,10 +3,12 @@
 # line with these 2 lines:
 #   require 'rubygems'
 #   require 'rbook/gbip'
-require File.dirname(__FILE__) + '/../lib/rbook/gbip'
+
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
+require 'rbook/gbip'
 
 gbip = RBook::GBIP::POS.new("username", "password")
 
 #puts gbip.find("0091835135").inspect
-puts gbip.find("1741146712").inspect
+puts gbip.find(:first, "1741146712").inspect
 
